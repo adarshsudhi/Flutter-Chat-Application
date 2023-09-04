1e72ef6564890ef5ddd12f713ed7014743c35274
Adarsh N S <59518674+adarshsudhi@users.noreply.github.com>
2023-09-04 20:06:47 +0530

    Removed Some Server key

--- lib/config/constants/constants.dart
+++ lib/config/constants/constants.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 
-String ServerKey = 'AAAAy3rSMgA:APA91bEbq96coCF5gTiasszFZAzBrnfGI1biaOwbeIKiNqFLTLBAvHM_8WWkm7NhgH0e1-EWn1G_wa4djfTBYjMPqioZFpN6flP1TKfrICCoz3XFWweH7d53CDetyhZ5bQ08XNxYI2Dg';
-String AppCertificate = '862e6112230640808517e94ea45a9406';
-String AppId = 'd6e2285f6e0340839e18c4fecf3ddc46';
+String ServerKey = 'Your Google Api Server Key';
+String AppCertificate = 'Enter the Agora AppCertificate';
+String AppId = 'Agora rtc AppID';
 String Baseurl = 'http://192.168.18.49:3000/generate/rtc';
 
 String Endpoint(String channelname,String uid){
